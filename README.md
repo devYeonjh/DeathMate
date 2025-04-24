57fc7b26afd89818a6ec32a1bc41b0f8946e5b64
Yeonjh <devYeonjh@gmail.com>
2025-04-24 16:06:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DeathMate
+
+Developed with Unreal Engine 5
---
+++ Source/DeathMate/DeathMate.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeathMate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeathMate, "DeathMate" );
