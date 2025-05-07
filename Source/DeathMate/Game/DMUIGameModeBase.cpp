46c65247d39a7a08df123b5c593fb19fc72acd6a
ehgnsdl <kwonehgnsthwjs@gmail.com>
2025-05-07 12:34:05 +0900

    스타트 UI 구현

---
+++ Source/DeathMate/Game/DMUIGameModeBase.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "DMUIGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/StageSelectWidget.h"
+#include "UI/StartScreenWidget.h"
+
+void ADMUIGameModeBase::BeginPlay()
+{
+    if (StartScreenWidgetClass)  // UPROPERTY(EditDefaultsOnly) 로 노출
+    {
+        UUserWidget* StartScreen = CreateWidget<UUserWidget>(GetWorld(), StartScreenWidgetClass);
+        if (StartScreen)
+        {
+            StartScreen->AddToViewport(/*ZOrder=*/ 0);
+           
+        }
+    }
+}
