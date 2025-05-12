fafe5a87674c69cf8451ef5e6375c5c51ba217b8
ehgnsdl <kwonehgnsthwjs@gmail.com>
2025-05-12 13:53:17 +0900

    Stage Goal, end UI 제작 및 적용

---
+++ Source/DeathMate/UI/EndscreenWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EndscreenWidget.generated.h"
+
+class UButton;
+
+/**
+ * 메인 로비(StartScreen) 위젯을 C++로 구현한 클래스
+ */
+UCLASS()
+class DEATHMATE_API UEndscreenWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+    TSubclassOf<UUserWidget> StartScreenWidgetClass;
+protected:
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* ExitButton;
+    UFUNCTION()
+    void OnExitClicked();
+};
